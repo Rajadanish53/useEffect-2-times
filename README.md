ca7eb0b73f2bbc47bc1f129f4608b04b63b8a176
Rajadanish53 <63229506+Rajadanish53@users.noreply.github.com>
2022-09-21 15:15:29 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-8rveqy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-8rveqy)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  React.useEffect(() => {
+    console.log('running');
+  }, []);
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
